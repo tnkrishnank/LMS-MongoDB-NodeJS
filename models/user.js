a2ae6060b16f7fd9ab410232ea361fae122571b0
NAVIN KRISHNA <81145975+tnkrishnank@users.noreply.github.com>
2022-03-19 19:12:23 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -10,8 +10,7 @@ const userSchema = new Schema({
         required: true
     },
     userImg: {
-        data: Buffer,
-        contentType: String
+        type: String
     },
     email: {
         type: String,
